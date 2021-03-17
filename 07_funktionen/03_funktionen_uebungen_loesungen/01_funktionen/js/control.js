0d0a8fceaef38a48a68e9445d2316c8d0597f4d7
Witzelfitz <Witzelfitz@gmail.com>
2021-03-17 09:13:04 +0100

    Drei-Button Lösung eingefügt

--- 07_funktionen/03_funktionen_uebungen_loesungen/01_funktionen/js/control.js
+++ 07_funktionen/03_funktionen_uebungen_loesungen/01_funktionen/js/control.js
@@ -3,7 +3,7 @@
 			console.log("Hallo Welt");
 			gutGemacht();
 		}
-		
+
 		//Einzelarbeit - drei funktionen bauen, die sich aufrufen
 		function gutGemacht(){
 			console.log("Gut gemacht")
@@ -11,17 +11,20 @@
 		}
 		function fertig(){
 			console.log("Fertig")
-			
+
 		}
-		
+
+		function nachrichtAusgeben(dieNachricht){
+			console.log(dieNachricht);
+		}
 		/*Beispiel um Variabeln zu übergeben
-			
+
 		function HalloWelt(){
 			console.log("Hallo Welt");
 			var text1 = "Gut gemacht";
 			gutGemacht(text1);
 		}
-		
+
 		//Einzelarbeit - drei funktionen bauen, die sich aufrufen
 		function gutGemacht(text){
 			console.log(text)
@@ -31,26 +34,26 @@
 		}
 		function fertig(text){
 			console.log(text)
-			
+
 		}
-			
+
 			*/
-		
-		
+
+
 		//Erstelle drei Buttons und bei jedem Button erscheint ein andere Alert. Es muss aber immer die gleiche Funktion aufgerufen werden
-			
+
 
 /*
 			<script>
 				//Listener bauen
 		document.getElementById("zeitButton").addEventListener("click", zeigeDatum);
-		
+
 		//passende funktion bauen
 		function zeigeDatum() {
 		  document.getElementById("datum").innerHTML = Date();
 		}
-		
-		
+
+
 
 
 			</script>
