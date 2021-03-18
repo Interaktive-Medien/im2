8ee6dbf3b4d9786697638363fb3e3f119456fb7f
Wolfgang Bock <wolfgang.bock@htwchur.ch>
2021-03-18 11:14:50 +0100

    Merge branch 'main' of https://github.com/Interaktive-Medien/im2 into main
    * 'main' of https://github.com/Interaktive-Medien/im2:
      Testcommit
      06_schleifen zur index.html hinzugefuegt
      Drei-Button Lösung eingefügt
      fix Pluralisator Loesung
    
    # Conflicts:
    #	index.html

--- 07_funktionen/03_funktionen_uebungen_loesungen/01_funktionen/js/control.js
+++ 07_funktionen/03_funktionen_uebungen_loesungen/01_funktionen/js/control.js
@@ -4,6 +4,7 @@
 			gutGemacht();
 		}
 		
+
 		//Einzelarbeit - drei funktionen bauen, die sich aufrufen
 		function gutGemacht(){
 			console.log("Gut gemacht")
@@ -12,16 +13,23 @@
 		function fertig(){
 			console.log("Fertig")
 			
+
+		}
+
+		function nachrichtAusgeben(dieNachricht){
+			console.log(dieNachricht);
 		}
 		
 		/*Beispiel um Variabeln zu übergeben
 			
+
 		function HalloWelt(){
 			console.log("Hallo Welt");
 			var text1 = "Gut gemacht";
 			gutGemacht(text1);
 		}
 		
+
 		//Einzelarbeit - drei funktionen bauen, die sich aufrufen
 		function gutGemacht(text){
 			console.log(text)
@@ -32,19 +40,25 @@
 		function fertig(text){
 			console.log(text)
 			
+
 		}
 			
+
 			*/
 		
 		
+
+
 		//Erstelle drei Buttons und bei jedem Button erscheint ein andere Alert. Es muss aber immer die gleiche Funktion aufgerufen werden
 			
+
 
 /*
 			<script>
 				//Listener bauen
 		document.getElementById("zeitButton").addEventListener("click", zeigeDatum);
 		
+
 		//passende funktion bauen
 		function zeigeDatum() {
 		  document.getElementById("datum").innerHTML = Date();
@@ -53,5 +67,8 @@
 		
 
 
+
+
 			</script>
-*/
+*/*/*/
+*/*/
