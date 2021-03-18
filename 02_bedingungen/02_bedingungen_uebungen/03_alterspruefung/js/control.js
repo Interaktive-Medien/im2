476e5aae66b7369f7369ea6655aa3fc7b4615749
Nick Schneeberger <nick.schneeberger@icloud.com>
2021-03-18 14:45:15 +0100

    delete Pluralisator create 03_alterspruefung

---
+++ 02_bedingungen/02_bedingungen_uebungen/03_alterspruefung/js/control.js
@@ -0,0 +1,11 @@
+// User geben Getränk und Alter in einem Prompt-Fenster ein.
+// Prüfe, ob das entsprechende Getränk im angegeben Alter
+// schon gekauft werden darf. Gib das Resultat in der Konsole aus.
+
+// Prüfe folgende Getränke:
+// Cola ab 0 Jahren
+// Bier ab 16 Jahren
+// Schnaps ab 18 Jahren
+
+let drink = prompt("Möchtest du Cola, Bier oder Schnaps kaufen?");
+let alter = prompt("Wie alt bist du?");
