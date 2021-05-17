b39f9544be5283369e8ac3ff6e5363e9ee0e0f17
marcfehr <marc.fehr@gmail.com>
2021-05-17 09:51:40 +0200

    Adds comments for data structure change for Sweden

--- 30_datenvisualisierung/03-uebungen/05-highcharts-bar-chart/01-uebungsdateien/js/script.js
+++ 30_datenvisualisierung/03-uebungen/05-highcharts-bar-chart/01-uebungsdateien/js/script.js
@@ -1,1 +1,11 @@
-console.log('script.js wurde rfolgreich geladen');
+console.log('script.js wurde erfolgreich geladen');
+
+console.log('⚠️ Achtung, bitte Hinweis im ~/js/script.js beachten! ⚠️')
+
+// Wichtige Information für den Kurs 2021:
+
+// Falls Ihr hier die aktuellen Zahlen (zB. 05-05-2021.csv) von dem Server laden möchtet, müsst ihr ein anderes
+// Land als 'Sweden' wählen, da die Datenstruktur der CSV-Dateien geändert haben. Solche Anpassungen in der Struktur
+// einer Datenquelle sind nicht unüblich, deshalb ist das ein gutes Beispiel für die Flexibilität, die man in der
+// Programmierung solcher Datenvisualisierungen aufbringen muss. Mit den Daten 05-05-2020.csv allerdings wird
+// der untenstehende Code mit 'Sweden' als Key für das zweite Land funktionieren.
--- 30_datenvisualisierung/03-uebungen/06-highcharts-zusatz-uebungen/01-uebungsdateien/js/script.js
+++ 30_datenvisualisierung/03-uebungen/06-highcharts-zusatz-uebungen/01-uebungsdateien/js/script.js
@@ -1,5 +1,15 @@
 console.log('script.js wurde erfolgreich geladen');
 
+console.log('⚠️ Achtung, bitte Hinweis im ~/js/script.js beachten! ⚠️')
+
+// Wichtige Information für den Kurs 2021:
+
+// Falls Ihr hier die aktuellen Zahlen (zB. 05-05-2021.csv) von dem Server laden möchtet, müsst ihr ein anderes
+// Land als 'Sweden' wählen, da die Datenstruktur der CSV-Dateien geändert haben. Solche Anpassungen in der Struktur
+// einer Datenquelle sind nicht unüblich, deshalb ist das ein gutes Beispiel für die Flexibilität, die man in der
+// Programmierung solcher Datenvisualisierungen aufbringen muss. Mit den Daten 05-05-2020.csv allerdings wird
+// der untenstehende Code mit 'Sweden' als Key für das zweite Land funktionieren.
+
 let datenQuelleVonGithubAlsCSV = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/05-05-2020.csv';
 let geparsteCSVDaten = Papa.parse(datenQuelleVonGithubAlsCSV, {
 	download: true,
